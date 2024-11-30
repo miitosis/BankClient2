c5973d26cd1c681cf8780cb7e2f2a855c56552a0
Kaneki0214 <najibtahiri0214@gmail.com>
2024-11-30 17:13:34 -0500

    1.1 fait

--- src/main/java/com/programmes/MainFrame.java
+++ src/main/java/com/programmes/MainFrame.java
@@ -94,6 +94,15 @@ public class MainFrame extends JFrame implements Runnable, Observateur {
         if (observable instanceof Client) {
             Client client = (Client)observable;
             if (!client.isConnecte()) {
+                // Le client est connecté
+                miConnecter.setEnabled(false);   // Désactiver l'item "Se connecter"
+                miDeconnecter.setEnabled(true);  // Activer l'item "Se déconnecter"
+                this.setTitle(TITRE + " - Connecté");
+                panneauPrincipal.setVisible(true);
+            }else{
+                // Le client est déconnecté
+                miConnecter.setEnabled(true);    // Activer l'item "Se connecter"
+                miDeconnecter.setEnabled(false); // Désactiver l'item "Se déconnecter"
                 this.setTitle(TITRE);
                 this.panneauPrincipal.setVisible(false);
                 panneauPrincipal.cacherPanneauCompteClient();
